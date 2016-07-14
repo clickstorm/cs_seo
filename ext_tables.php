f14df4fbb2c631827405f1cc6407803903a60f15
mhirdes <hirdes@clickstorm.de>
2016-07-14 09:27:43 +0200

    [BUGFIX] #6 check if DB is initialized

--- ext_tables.php
+++ ext_tables.php
@@ -35,42 +35,47 @@ if (TYPO3_MODE === 'BE') {
         )
     );
 }
-// Add TCA Field
-$tempColumns = [
-    'tx_csseo' => [
-        'exclude' => 0,
-        'label' => 'LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:tx_csseo_domain_model_meta',
-        'config' => [
-            'type' => 'inline',
-            'foreign_table' => 'tx_csseo_domain_model_meta',
-            'foreign_field' => 'uid_foreign',
-            'foreign_table_field' => 'tablenames',
-            'maxitems' => 1,
-            'appearance' => [
-                'collapseAll' => false,
-                'showPossibleLocalizationRecords' => true,
-                'showRemovedLocalizationRecords' => true,
-                'showSynchronizationLink' => true,
-            ],
-            'behaviour' => [
-                'localizationMode' => 'select',
-                'localizeChildrenAtParentLocalization' => TRUE,
-            ],
-        ],
-    ]
-];
 
-$pageTS = \TYPO3\CMS\Backend\Utility\BackendUtility::getPagesTSconfig(1);
-if($pageTS['tx_csseo.']) {
-    foreach ($pageTS['tx_csseo.'] as $table) {
-        if(is_string($table)) {
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table,$tempColumns,1);
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
-                $table,
-                '--div--;LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:pages.tab.seo,tx_csseo'
-            );
-        }
-    }
+// Add TCA Field
+if(isset($GLOBALS['TYPO3_DB'])) {
+
+	$tempColumns = [
+		'tx_csseo' => [
+			'exclude' => 0,
+			'label' => 'LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:tx_csseo_domain_model_meta',
+			'config' => [
+				'type' => 'inline',
+				'foreign_table' => 'tx_csseo_domain_model_meta',
+				'foreign_field' => 'uid_foreign',
+				'foreign_table_field' => 'tablenames',
+				'maxitems' => 1,
+				'appearance' => [
+					'collapseAll' => false,
+					'showPossibleLocalizationRecords' => true,
+					'showRemovedLocalizationRecords' => true,
+					'showSynchronizationLink' => true,
+				],
+				'behaviour' => [
+					'localizationMode' => 'select',
+					'localizeChildrenAtParentLocalization' => TRUE,
+				],
+			],
+		]
+	];
+
+	$pageTS = \TYPO3\CMS\Backend\Utility\BackendUtility::getPagesTSconfig(1);
+
+	if($pageTS['tx_csseo.']) {
+		foreach ($pageTS['tx_csseo.'] as $table) {
+			if(is_string($table)) {
+				\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table,$tempColumns,1);
+				\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+					$table,
+					'--div--;LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:pages.tab.seo,tx_csseo'
+				);
+			}
+		}
+	}
 }
 
 // register Ajax Handler
