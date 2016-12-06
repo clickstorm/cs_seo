84a7906c5e7075dbb868e3f762d68aacf187a8e5
mhirdes <hirdes@clickstorm.de>
2016-12-06 16:23:38 +0100

    [BUGFIX] recursvie folder and no index

--- Classes/UserFunc/Sitemap.php
+++ Classes/UserFunc/Sitemap.php
@@ -40,8 +40,7 @@ use TYPO3\CMS\Frontend\Page\PageRepository;
  * Class Sitemap
  * @package Clickstorm\CsSeo\UserFunc
  */
-class Sitemap
-{
+class Sitemap {
 	/**
 	 * @var array
 	 */
@@ -87,13 +86,28 @@ class Sitemap
 				);
 				$settings = $this->settings['pages'];
 
-				$pages[] = $this->tsfe->sys_page->getPage($settings['rootPid']);
-				$pages = $this->getPages($pages, $pages);
+				// first get the root page
+				$rootPage = $this->tsfe->sys_page->getPage($settings['rootPid']);
 
-				$this->view->assignMultiple([
-					'lang' => $this->tsfe->sys_language_uid,
-					'pages' => $pages
-				]);
+				// remove doktype exlude
+				$this->tsfe->sys_page->where_hid_del = str_replace(
+					' AND pages.doktype<200',
+					'',
+					$this->tsfe->sys_page->where_hid_del
+				);
+
+				// get the subpages
+				$subPages = $this->getSubPages($rootPage['uid']);
+
+				// merge all pages
+				$pages = array_merge([$rootPage], $subPages);
+
+				$this->view->assignMultiple(
+					[
+						'lang' => $this->tsfe->sys_language_uid,
+						'pages' => $pages
+					]
+				);
 				break;
 			// sitemap for extensions
 			case 'extension':
@@ -114,7 +128,7 @@ class Sitemap
 						foreach ($records as $key => $record) {
 							$typoLinkConf['additionalParams'] = '&' . $extConf['additionalParams'] . '=' . $record['uid'];
 							if ($record['lang']) {
-								$typoLinkConf['additionalParams'] .= '&L=' . $record['langlang'];
+								$typoLinkConf['additionalParams'] .= '&L=' . $record['lang'];
 							}
 							$records[$key]['loc'] = $cObject->typoLink_URL($typoLinkConf);
 						}
@@ -122,126 +136,24 @@ class Sitemap
 						$this->view->assign('records', $records);
 					}
 				}
-		    	break;
+				break;
 			// list all sitemaps
-		    default: $this->view->setTemplatePathAndFilename($absoluteResourcesPath . 'Private/Templates/Sitemap/ListAll.xml');
-	                $this->view->assign('settings', $this->settings);
-	    }
+			default:
+				$this->view->setTemplatePathAndFilename(
+					$absoluteResourcesPath . 'Private/Templates/Sitemap/ListAll.xml'
+				);
+				$this->view->assign('settings', $this->settings);
+		}
 
 
-	    return $this->view->render();
-    }
-
-	/**
-	 * @param array $pages
-	 * @param array $newPages
-	 * @return array
-	 */
-	protected function getPages($pages, $newPages = []) {
-		$subPages = $this->tsfe->sys_page->getMenu(
-			array_column($newPages,'uid'),
-			'*',
-			'sorting',
-			'AND doktype != 6 AND tx_csseo_no_index = 0'
-		);
-
-		return $subPages ? $this->getPages(array_merge($pages, $subPages), $subPages) : $pages;
-    }
-
-	/**
-	 * @param array $extConf
-	 * @return bool|array
-	 */
-    protected function getRecords($extConf) {
-	    $db = $this->getDatabaseConnection();
-	    if (!isset($GLOBALS['TCA'][$extConf['table']])) {
-	    	return false;
-	    }
-
-	    $table = $extConf['table'];
-	    $where = '';
-	    $from = $extConf['table'];
-	    $select = $table . '.uid';
-
-	    $constraints = [];
-	    $lang = $this->getTypoScriptFrontendController()->sys_language_uid;
-	    $tca = $GLOBALS['TCA'][$extConf['table']];
-
-	    // storage
-	    if($extConf['storagePid']) {
-		    $constraints[] = $table . '.pid IN (' . $extConf['storagePid'] . ')';
-	    }
-
-	    // lang
-        $languageField = $tca['ctrl']['languageField'];
-        if($languageField) {
-		    $constraints[] = $table . '.' .$languageField . ' IN (' . $lang . ',-1)';
-		    $select .= ', ' . $table . '.' .$languageField . ' AS lang';
-	    }
-
-	    // categories
-	    if($extConf['categories']) {
-        	if($extConf['categoryField']) {
-		        $constraints[] = $extConf['categoryField'] . ' IN (' . $extConf['categories'] . ')';
-	        } elseif($extConf['categoryMMTable']) {
-		        $catTable = $extConf['categoryMMTable'];
-		        $from .= ', ' . $catTable;
-		        $constraints[] = $table . '.uid = ' . $catTable .'.uid_foreign';
-		        $constraints[] = $catTable. '.uid_local IN (' . $extConf['categories'] . ')';
-		        if($extConf['categoryMMTablename']) {
-			        $constraints[] = $catTable . '.tablenames = ' . $db->fullQuoteStr($table, $table);
-		        }
-		        if($extConf['categoryMMFieldname']) {
-			        $constraints[] = $catTable.'.fieldname = ' . $db->fullQuoteStr($extConf['categoryMMFieldname'], $table);
-		        }
-	        }
-
-        }
-
-        // lastmod
-	    if($tca['ctrl']['tstamp']) {
-		    $select .= ', ' . $table . '.' . $tca['ctrl']['tstamp'] . ' AS lastmod';
-	    }
-
-	    // no index
-	    if($tca['columns']['tx_csseo']) {
-        	$from .= ', tx_csseo_domain_model_meta';
-        	$constraints[] = '('. $table . '.tx_csseo = 0 OR
-        	 (' . $table . '.uid = tx_csseo_domain_model_meta.uid_foreign AND
-        	 tx_csseo_domain_model_meta.tablenames = ' . $db->fullQuoteStr($table, $table) . ' AND
-        	 tx_csseo_domain_model_meta.no_index = 0))';
-	    }
-
-	    if(count($constraints)) {
-		    $where .= implode($constraints, ' AND ');
-	    } else {
-        	$where = '1=1';
-	    }
-
-	    $where .= $this->pageRepository->enableFields(
-		    $extConf['table']
-	    );
-
-	    return $db->exec_SELECTgetRows(
-		    $select,
-		    $from,
-		    $where,
-		    $table . '.uid'
-	    );
-    }
-
-	/**
-	 * @return array
-	 */
-	public function getSettings() {
-		return $this->settings;
+		return $this->view->render();
 	}
 
 	/**
-	 * @param array $settings
+	 * @return TypoScriptFrontendController
 	 */
-	public function setSettings($settings) {
-		$this->settings = $settings;
+	protected function getTypoScriptFrontendController() {
+		return $GLOBALS['TSFE'];
 	}
 
 	/**
@@ -252,7 +164,7 @@ class Sitemap
 	 */
 	protected function parseSettings($settings) {
 		$parsedSettings = [];
-		if(is_array($settings)) {
+		if (is_array($settings)) {
 			foreach ($settings as $key => $value) {
 				$key = rtrim($key, '.');
 				if (!is_array($value)) {
@@ -266,29 +178,112 @@ class Sitemap
 		return $parsedSettings;
 	}
 
-	protected function getPageConstraints($includeNotInMenu = false, $includeMenuSeparator = false)
-	{
-		$constraints = [];
+	/**
+	 * @param array $pages
+	 * @param array $newPages
+	 * @return array
+	 */
+	protected function getSubPages($pageUid) {
+		$subPages = $this->tsfe->sys_page->getMenu(
+			$pageUid,
+			'*',
+			'sorting',
+			''
+		);
 
-		$constraints[] = 'doktype = 1';
-
-		if (!$includeNotInMenu) {
-			$constraints[] = 'nav_hide = 0';
+		foreach ($subPages as $subPage) {
+			ArrayUtility::mergeRecursiveWithOverrule(
+				$subPages,
+				$this->getSubPages($subPage['uid'])
+			);
 		}
 
-		if (!$includeMenuSeparator) {
-			$constraints[] = 'doktype != ' . PageRepository::DOKTYPE_SPACER;
-		}
-
-		return 'AND ' . implode(' AND ', $constraints);
+		return $subPages;
 	}
 
 	/**
-	 * @return TypoScriptFrontendController
+	 * @param array $extConf
+	 * @return bool|array
 	 */
-	protected function getTypoScriptFrontendController()
-	{
-		return $GLOBALS['TSFE'];
+	protected function getRecords($extConf) {
+		$db = $this->getDatabaseConnection();
+		if (!isset($GLOBALS['TCA'][$extConf['table']])) {
+			return false;
+		}
+
+		$table = $extConf['table'];
+		$where = '';
+		$from = $extConf['table'];
+		$select = $table . '.uid';
+
+		$constraints = [];
+		$lang = $this->getTypoScriptFrontendController()->sys_language_uid;
+		$tca = $GLOBALS['TCA'][$extConf['table']];
+
+		// storage
+		if ($extConf['storagePid']) {
+			$constraints[] = $table . '.pid IN (' . $extConf['storagePid'] . ')';
+		}
+
+		// lang
+		$languageField = $tca['ctrl']['languageField'];
+		if ($languageField) {
+			$constraints[] = $table . '.' . $languageField . ' IN (' . $lang . ',-1)';
+			$select .= ', ' . $table . '.' . $languageField . ' AS lang';
+		}
+
+		// categories
+		if ($extConf['categories']) {
+			if ($extConf['categoryField']) {
+				$constraints[] = $extConf['categoryField'] . ' IN (' . $extConf['categories'] . ')';
+			} elseif ($extConf['categoryMMTable']) {
+				$catTable = $extConf['categoryMMTable'];
+				$from .= ', ' . $catTable;
+				$constraints[] = $table . '.uid = ' . $catTable . '.uid_foreign';
+				$constraints[] = $catTable . '.uid_local IN (' . $extConf['categories'] . ')';
+				if ($extConf['categoryMMTablename']) {
+					$constraints[] = $catTable . '.tablenames = ' . $db->fullQuoteStr($table, $table);
+				}
+				if ($extConf['categoryMMFieldname']) {
+					$constraints[] = $catTable . '.fieldname = ' . $db->fullQuoteStr(
+							$extConf['categoryMMFieldname'],
+							$table
+						);
+				}
+			}
+
+		}
+
+		// lastmod
+		if ($tca['ctrl']['tstamp']) {
+			$select .= ', ' . $table . '.' . $tca['ctrl']['tstamp'] . ' AS lastmod';
+		}
+
+		// no index
+		if ($tca['columns']['tx_csseo']) {
+			$from .= ', tx_csseo_domain_model_meta';
+			$constraints[] = '(' . $table . '.tx_csseo = 0 OR
+        	 (' . $table . '.uid = tx_csseo_domain_model_meta.uid_foreign AND
+        	 tx_csseo_domain_model_meta.tablenames = ' . $db->fullQuoteStr($table, $table) . ' AND
+        	 tx_csseo_domain_model_meta.no_index = 0))';
+		}
+
+		if (count($constraints)) {
+			$where .= implode($constraints, ' AND ');
+		} else {
+			$where = '1=1';
+		}
+
+		$where .= $this->pageRepository->enableFields(
+			$extConf['table']
+		);
+
+		return $db->exec_SELECTgetRows(
+			$select,
+			$from,
+			$where,
+			$table . '.uid'
+		);
 	}
 
 	/**
@@ -296,8 +291,21 @@ class Sitemap
 	 *
 	 * @return \TYPO3\CMS\Core\Database\DatabaseConnection
 	 */
-	protected function getDatabaseConnection()
-	{
+	protected function getDatabaseConnection() {
 		return $GLOBALS['TYPO3_DB'];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getSettings() {
+		return $this->settings;
+	}
+
+	/**
+	 * @param array $settings
+	 */
+	public function setSettings($settings) {
+		$this->settings = $settings;
 	}
 }
