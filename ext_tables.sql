68ee4040f1543dc0a83b46225ee73826401e3601
Marc Hirdes <hirdes@clickstorm.de>
2017-03-29 21:58:24 +0200

    [BUGFIX] #56 add image translation fields to db for correct translation handling

--- ext_tables.sql
+++ ext_tables.sql
@@ -27,8 +27,10 @@ CREATE TABLE pages_language_overlay (
   tx_csseo_canonical varchar(255) DEFAULT '' NOT NULL,
   tx_csseo_og_title varchar(255) DEFAULT '' NOT NULL,
   tx_csseo_og_description text NOT NULL,
+  tx_csseo_og_image int(11) unsigned NOT NULL default '0',
   tx_csseo_tw_title varchar(255) DEFAULT '' NOT NULL,
   tx_csseo_tw_description text NOT NULL,
+  tx_csseo_tw_image int(11) unsigned NOT NULL default '0',
   tx_csseo_tw_creator varchar(255) DEFAULT '' NOT NULL
 );
 
