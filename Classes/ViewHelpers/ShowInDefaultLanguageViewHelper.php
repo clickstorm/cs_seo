dc1ad4e6818d95451465a2bbbb68ae4f6817b847
Daniel Goerz <dlg@lightwerk.com>
2017-08-31 13:56:02 +0200

    [BUGFIX] Do not include pages in sitemap that are hidden in default language

---
+++ Classes/ViewHelpers/ShowInDefaultLanguageViewHelper.php
@@ -0,0 +1,53 @@
+<?php
+namespace Clickstorm\CsSeo\ViewHelpers;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2017 Daniel Goerz <ervaude@gmail.com>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class IsInListViewHelper
+ *
+ * @package Clickstorm\CsSeo\ViewHelpers
+ */
+class ShowInDefaultLanguageViewHelper extends AbstractViewHelper
+{
+
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('fieldValue', 'int', 'The value of the l18n_cfg field of the pages table.', true);
+    }
+
+    /**
+     * @return bool
+     */
+    public function render()
+    {
+        return !GeneralUtility::hideIfDefaultLanguage((int)$this->arguments['fieldValue']);
+    }
+}
