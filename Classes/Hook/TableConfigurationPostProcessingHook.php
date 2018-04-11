4d38baa04343c342cf9a84d2420b8d041682d023
mhirdes <hirdes@clickstorm.de>
2018-04-11 14:25:36 +0200

    [BUGFIX] remove TYPO3_DB

--- Classes/Hook/TableConfigurationPostProcessingHook.php
+++ Classes/Hook/TableConfigurationPostProcessingHook.php
@@ -75,41 +75,38 @@ class TableConfigurationPostProcessingHook implements TableConfigurationPostProc
         );
         
         // Extend TCA of records like news etc.
-        if(isset($GLOBALS['TYPO3_DB'])) {
-
-            $tempColumns = [
-                'tx_csseo' => [
-                    'exclude' => 0,
-                    'label' => 'LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:tx_csseo_domain_model_meta',
-                    'config' => [
-                        'type' => 'inline',
-                        'foreign_table' => 'tx_csseo_domain_model_meta',
-                        'foreign_field' => 'uid_foreign',
-                        'foreign_table_field' => 'tablenames',
-                        'maxitems' => 1,
-                        'appearance' => [
-                            'collapseAll' => false,
-                            'showPossibleLocalizationRecords' => true,
-                            'showRemovedLocalizationRecords' => true,
-                            'showSynchronizationLink' => true,
-                        ],
-                        'behaviour' => [
-                            'localizationMode' => 'select',
-                            'localizeChildrenAtParentLocalization' => TRUE,
-                        ],
+        $tempColumns = [
+            'tx_csseo' => [
+                'exclude' => 0,
+                'label' => 'LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:tx_csseo_domain_model_meta',
+                'config' => [
+                    'type' => 'inline',
+                    'foreign_table' => 'tx_csseo_domain_model_meta',
+                    'foreign_field' => 'uid_foreign',
+                    'foreign_table_field' => 'tablenames',
+                    'maxitems' => 1,
+                    'appearance' => [
+                        'collapseAll' => false,
+                        'showPossibleLocalizationRecords' => true,
+                        'showRemovedLocalizationRecords' => true,
+                        'showSynchronizationLink' => true,
                     ],
-                ]
-            ];
+                    'behaviour' => [
+                        'localizationMode' => 'select',
+                        'localizeChildrenAtParentLocalization' => TRUE,
+                    ],
+                ],
+            ]
+        ];
 
-            $tables = ConfigurationUtility::getTablesToExtend();
-            if($tables) {
-                foreach ($tables as $table) {
-                    ExtensionManagementUtility::addTCAcolumns($table,$tempColumns);
-                    ExtensionManagementUtility::addToAllTCAtypes(
-                        $table,
-                        '--div--;LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:pages.tab.seo,tx_csseo'
-                    );
-                }
+        $tables = ConfigurationUtility::getTablesToExtend();
+        if($tables) {
+            foreach ($tables as $table) {
+                ExtensionManagementUtility::addTCAcolumns($table,$tempColumns);
+                ExtensionManagementUtility::addToAllTCAtypes(
+                    $table,
+                    '--div--;LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:pages.tab.seo,tx_csseo'
+                );
             }
         }
     }
