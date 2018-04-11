a3ba0a6e1a37f81a0fcc9854f294b39ec770fbc1
mhirdes <hirdes@clickstorm.de>
2018-04-11 14:47:26 +0200

    [BUGFIX] migrate ext update

--- class.ext_update.php
+++ class.ext_update.php
@@ -15,9 +15,12 @@ namespace Clickstorm\CsSeo;
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Query;
 
 /**
  * Update class for the extension manager.
@@ -25,195 +28,222 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  * @package TYPO3
  * @subpackage tx_csseo
  */
-class ext_update {
+class ext_update
+{
 
-	const FOLDER_CATEGORY_IMAGES = '/_migrated/news_categories';
+    const FOLDER_CATEGORY_IMAGES = '/_migrated/news_categories';
 
-	/**
-	 * Array of flash messages (params) array[][status,title,message]
-	 *
-	 * @var array
-	 */
-	protected $messageArray = [];
+    /**
+     * Array of flash messages (params) array[][status,title,message]
+     *
+     * @var array
+     */
+    protected $messageArray = [];
 
-	/**
-	 * @var \TYPO3\CMS\Core\Database\DatabaseConnection
-	 */
-	protected $databaseConnection;
+    /**
+     * Main update function called by the extension manager.
+     *
+     * @return string
+     */
+    public function main()
+    {
+        $this->processUpdates();
 
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
-		$this->databaseConnection = $GLOBALS['TYPO3_DB'];
-	}
+        return $this->generateOutput();
+    }
 
-	/**
-	 * Main update function called by the extension manager.
-	 *
-	 * @return string
-	 */
-	public function main() {
-		$this->processUpdates();
-		return $this->generateOutput();
-	}
+    /**
+     * The actual update function. Add your update task in here.
+     *
+     * @return void
+     */
+    protected function processUpdates()
+    {
+        $this->migrateFromMetaseo();
+        $this->migrateFromSeoBasics();
+    }
 
-	/**
-	 * Called by the extension manager to determine if the update menu entry
-	 * should by showed.
-	 *
-	 * @return bool
-	 * @todo find a better way to determine if update is needed or not.
-	 */
-	public function access() {
-		return TRUE;
-	}
+    /**
+     * Check if metaseo was installed and then transfer the properties from pages and pages_language_overlay
+     *
+     * @return void
+     */
+    protected function migrateFromMetaseo()
+    {
+        // check if tx_metaseo fields exists
+        if (!array_key_exists('tx_metaseo_pagetitle', $this->getPageColumns())) {
+            $status = FlashMessage::NOTICE;
+            $title = 'metaseo not found';
+            $message = 'No MetaSeo properties, so no update needed';
+            $this->messageArray[] = [$status, $title, $message];
 
-	/**
-	 * The actual update function. Add your update task in here.
-	 *
-	 * @return void
-	 */
-	protected function processUpdates() {
-		$this->migrateFromMetaseo();
-		$this->migrateFromSeoBasics();
-	}
+            return;
+        }
 
-	/**
-	 * Check if metaseo was installed and then transfer the properties from pages and pages_language_overlay
-	 *
-	 * @return void
-	 */
-	protected function migrateFromMetaseo() {
+        // update title only if absolute title
+        $this->updateTitleOnly('tx_metaseo_pagetitle', 'pages');
 
-		// check if tx_metaseo fields exists
-		$metaseoTableFields = $this->databaseConnection->admin_get_fields('pages');
-		if (!array_key_exists('tx_metaseo_pagetitle', $metaseoTableFields)) {
-			$status = FlashMessage::NOTICE;
-			$title = 'metaseo not found';
-			$message = 'No MetaSeo properties, so no update needed';
-			$this->messageArray[] = [$status, $title, $message];
-			return;
-		}
+        // migrate pages
+        $fieldsToMigrate = [
+            'tx_metaseo_pagetitle' => 'tx_csseo_title',
+            'tx_metaseo_pagetitle_rel' => 'tx_csseo_title',
+            'tx_metaseo_canonicalurl' => 'tx_csseo_canonical',
+            'tx_metaseo_is_exclude' => 'tx_csseo_no_index',
+        ];
 
-		// update title only if absolute title
-		$this->updateTitleOnly('tx_metaseo_pagetitle', 'pages');
+        $this->migrateFields($fieldsToMigrate, 'pages');
 
-		// migrate pages
-		$fieldsToMigrate = [
-			'tx_metaseo_pagetitle'      => 'tx_csseo_title',
-			'tx_metaseo_pagetitle_rel'  => 'tx_csseo_title',
-			'tx_metaseo_canonicalurl'   => 'tx_csseo_canonical',
-			'tx_metaseo_is_exclude'     => 'tx_csseo_no_index',
-		];
+        // update title only if absolute title
+        $this->updateTitleOnly('tx_metaseo_pagetitle', 'pages_language_overlay');
 
-		$this->migrateFields($fieldsToMigrate, 'pages');
+        // migrate pages_language_overlay
+        $fieldsToMigrate = [
+            'tx_metaseo_pagetitle' => 'tx_csseo_title',
+            'tx_metaseo_pagetitle_rel' => 'tx_csseo_title',
+        ];
 
-		// update title only if absolute title
-		$this->updateTitleOnly('tx_metaseo_pagetitle', 'pages_language_overlay');
+        $this->migrateFields($fieldsToMigrate, 'pages_language_overlay');
 
-		// migrate pages_language_overlay
-		$fieldsToMigrate = [
-			'tx_metaseo_pagetitle'      => 'tx_csseo_title',
-			'tx_metaseo_pagetitle_rel'  => 'tx_csseo_title',
-		];
+        /**
+         * Finished migration from metaseo
+         */
+        $message = 'Title, Canonical and NoIndex are migrated. Run DB compare in the install tool to remove the fields from metaseo and run the DB check to update the reference index.';
+        $status = FlashMessage::OK;
+        $title = 'Migrated all metaseo fields!';
+        $this->messageArray[] = [$status, $title, $message];
+    }
 
-		$this->migrateFields($fieldsToMigrate, 'pages_language_overlay');
+    /**
+     * returns an array with all fields of the table pages
+     *
+     * @return mixed
+     */
+    protected function getPageColumns()
+    {
+        return GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable('pages')
+            ->getSchemaManager()
+            ->listTableColumns('pages');
+    }
 
-		/**
-		 * Finished migration from metaseo
-		 */
-		$message = 'Title, Canonical and NoIndex are migrated. Run DB compare in the install tool to remove the fields from metaseo and run the DB check to update the reference index.';
-		$status = FlashMessage::OK;
-		$title = 'Migrated all metaseo fields!';
-		$this->messageArray[] = [$status, $title, $message];
-	}
+    /**
+     * migrate the title only field, if absolute title-tag is set
+     * @param string $field
+     * @param string $table
+     */
+    protected function updateTitleOnly($field, $table)
+    {
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($table);
 
-	/**
-	 * Check if seo_basics was installed and then transfer the properties from pages and pages_language_overlay
-	 *
-	 * @return void
-	 */
-	protected function migrateFromSeoBasics() {
+        $queryBuilder
+            ->update($table, 'u')
+            ->where(
+                $queryBuilder->expr()->gt($field, '\'\'')
+            )
+            ->set('u.tx_csseo_title_only', 1)
+            ->execute();
+    }
 
-		// check if seo_basics fields exists
-		$metaseoTableFields = $this->databaseConnection->admin_get_fields('pages');
-		if (!array_key_exists('tx_seo_titletag', $metaseoTableFields)) {
-			$status = FlashMessage::NOTICE;
-			$title = 'seo_basics not found';
-			$message = 'No seo_basics properties, so no update needed';
-			$this->messageArray[] = [$status, $title, $message];
-			return;
-		}
+    /**
+     * Migrate fields from one column to another of a table
+     *
+     * @param array $fieldsToMigrate
+     * @param string $table
+     *
+     * @return void
+     */
+    protected function migrateFields($fieldsToMigrate, $table)
+    {
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($table);
+        foreach ($fieldsToMigrate as $oldField => $newField) {
 
-		// migrate pages
-		$fieldsToMigrate = [
-			'tx_seo_titletag'      => 'tx_csseo_title',
-			'tx_seo_canonicaltag'   => 'tx_csseo_canonical',
-		];
+            $queryBuilder
+                ->update($table, 'u')
+                ->where(
+                    $queryBuilder->expr()->orX(
+                        $queryBuilder->expr()->eq($newField, ''),
+                        $queryBuilder->expr()->isNull($newField)
+                    )
+                )
+                ->set('u.' . $newField, $queryBuilder->createNamedParameter('u.' . $oldField))
+                ->execute();
+        }
+    }
 
-		$this->migrateFields($fieldsToMigrate, 'pages');
-		$this->migrateFields($fieldsToMigrate, 'pages_language_overlay');
+    /**
+     * Check if seo_basics was installed and then transfer the properties from pages and pages_language_overlay
+     *
+     * @return void
+     */
+    protected function migrateFromSeoBasics()
+    {
 
-		/**
-		 * Finished migration from seo_basics
-		 */
-		$message = 'Title and Canonical are migrated. Run DB compare in the install tool to remove the fields from metaseo and run the DB check to update the reference index.';
-		$status = FlashMessage::OK;
-		$title = 'Migrated all seo_basics fields!';
-		$this->messageArray[] = [$status, $title, $message];
-	}
+        // check if seo_basics fields exists
+        if (!array_key_exists('tx_seo_titletag', $this->getPageColumns())) {
+            $status = FlashMessage::NOTICE;
+            $title = 'seo_basics not found';
+            $message = 'No seo_basics properties, so no update needed';
+            $this->messageArray[] = [$status, $title, $message];
 
-	/**
-	 * migrate the title only field, if absolute title-tag is set
-	 * @param string $field
-	 * @param string $table
-	 */
-	protected function updateTitleOnly($field, $table) {
-		// Settings from page
-		$query = 'UPDATE ' . $table . ' SET tx_csseo_title_only = 1';
-		$query .= ' WHERE ' . $field . ' > \'\'';
-		$GLOBALS['TYPO3_DB']->sql_query($query);
-	}
+            return;
+        }
 
-	/**
-	 * Migrate fields from one column to another of a table
-	 *
-	 * @param array $fieldsToMigrate
-	 * @param string $table
-	 *
-	 * @return void
-	 */
-	protected function migrateFields($fieldsToMigrate, $table) {
-		foreach($fieldsToMigrate as $oldField => $newField) {
-			// Settings from page
-			$query = 'UPDATE ' . $table . ' SET '.$newField.' = '.$oldField;
-			$query .= ' WHERE ' . $newField . ' = "" OR ' . $newField . ' IS NULL ';
-			$GLOBALS['TYPO3_DB']->sql_query($query);
-		}
-	}
+        // migrate pages
+        $fieldsToMigrate = [
+            'tx_seo_titletag' => 'tx_csseo_title',
+            'tx_seo_canonicaltag' => 'tx_csseo_canonical',
+        ];
 
+        $this->migrateFields($fieldsToMigrate, 'pages');
+        $this->migrateFields($fieldsToMigrate, 'pages_language_overlay');
 
-	/**
-	 * Generates output by using flash messages
-	 *
-	 * @return string
-	 */
-	protected function generateOutput() {
-	    /** @var FlashMessageService $flashMessageService */
+        /**
+         * Finished migration from seo_basics
+         */
+        $message = 'Title and Canonical are migrated. Run DB compare in the install tool to remove the fields from metaseo and run the DB check to update the reference index.';
+        $status = FlashMessage::OK;
+        $title = 'Migrated all seo_basics fields!';
+        $this->messageArray[] = [$status, $title, $message];
+    }
+
+    /**
+     * Generates output by using flash messages
+     *
+     * @return string
+     */
+    protected function generateOutput()
+    {
+        /** @var FlashMessageService $flashMessageService */
         $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
         $defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
 
-		foreach ($this->messageArray as $messageItem) {
-			/** @var \TYPO3\CMS\Core\Messaging\FlashMessage $flashMessage */
-			$flashMessage = GeneralUtility::makeInstance(
-				FlashMessage::class,
-				$messageItem[2],
-				$messageItem[1],
-				$messageItem[0]);
+        foreach ($this->messageArray as $messageItem) {
+            /** @var \TYPO3\CMS\Core\Messaging\FlashMessage $flashMessage */
+            $flashMessage = GeneralUtility::makeInstance(
+                FlashMessage::class,
+                $messageItem[2],
+                $messageItem[1],
+                $messageItem[0]);
             $defaultFlashMessageQueue->enqueue($flashMessage);
-		}
-		return $defaultFlashMessageQueue->renderFlashMessages();
-	}
+        }
+
+        return $defaultFlashMessageQueue->renderFlashMessages();
+    }
+
+    /**
+     * Called by the extension manager to determine if the update menu entry
+     * should by showed.
+     *
+     * @return bool
+     * @todo find a better way to determine if update is needed or not.
+     */
+    public function access()
+    {
+        return true;
+    }
 
 }
