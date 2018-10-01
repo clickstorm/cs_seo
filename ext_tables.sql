5f942430cf2b9711911e39eb65904279fafb9ace
mhirdes <hirdes@clickstorm.de>
2018-10-01 15:34:44 +0200

    [FEATURE] clean up ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,21 +2,10 @@
 # Table structure for table 'pages'
 #
 CREATE TABLE pages (
-  tx_csseo_title varchar(255) DEFAULT '' NOT NULL,
   tx_csseo_title_only tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	tx_csseo_keyword varchar(255) DEFAULT '' NOT NULL,
-  tx_csseo_canonical varchar(255) DEFAULT '' NOT NULL,
-  tx_csseo_no_index tinyint(1) unsigned DEFAULT '0' NOT NULL,
-  tx_csseo_no_follow tinyint(1) unsigned DEFAULT '0' NOT NULL,
-  tx_csseo_og_title varchar(255) DEFAULT '' NOT NULL,
-  tx_csseo_og_description text,
-  tx_csseo_og_image int(11) unsigned NOT NULL default '0',
-  tx_csseo_tw_title varchar(255) DEFAULT '' NOT NULL,
-  tx_csseo_tw_description text,
-  tx_csseo_tw_image int(11) unsigned NOT NULL default '0',
   tx_csseo_tw_creator varchar(255) DEFAULT '' NOT NULL,
   tx_csseo_tw_site varchar(255) DEFAULT '' NOT NULL
-
 );
 
 #
@@ -97,12 +86,6 @@ CREATE TABLE tx_csseo_domain_model_evaluation (
 
 	PRIMARY KEY (uid),
 	KEY parent (pid)
-);
-
-# Table structure for table "sys_domain"
-#
-CREATE TABLE sys_domain (
-	tx_csseo_robots_txt text
 );
 
 #
