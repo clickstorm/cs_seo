a2ec0756039a584b4d6161a33c90203011b6c417
mhirdes <hirdes@clickstorm.de>
2018-10-04 10:57:10 +0200

    [FEATURE] use auto create managed DB fields

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,10 +12,6 @@ CREATE TABLE pages (
 # Table structure for table 'tx_csseo_domain_model_meta'
 #
 CREATE TABLE tx_csseo_domain_model_meta (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	title varchar(255) DEFAULT '' NOT NULL,
 	title_only tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	keyword varchar(255) DEFAULT '' NOT NULL,
@@ -33,59 +29,18 @@ CREATE TABLE tx_csseo_domain_model_meta (
 	tw_site varchar(255) DEFAULT '' NOT NULL,
 
 	uid_foreign int(11) DEFAULT '0' NOT NULL ,
-	tablenames varchar(255) DEFAULT '' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+	tablenames varchar(255) DEFAULT '' NOT NULL
 );
 
 #
 # Table structure for table 'tx_csseo_domain_model_evaluation'
 #
 CREATE TABLE tx_csseo_domain_model_evaluation (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	results text,
 	url varchar(255) DEFAULT '' NOT NULL,
 
 	uid_foreign int(11) DEFAULT '0' NOT NULL ,
-	tablenames varchar(255) DEFAULT '' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+	tablenames varchar(255) DEFAULT '' NOT NULL
 );
 
 #
