dc3cfc9cc2b346b7ecdc245989dcc94c93529516
mhirdes <hirdes@clickstorm.de>
2020-01-31 17:49:29 +0100

    [BUGFIX] prevent error on backend login

--- ext_tables.php
+++ ext_tables.php
@@ -1,34 +1,39 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tx_csseo_domain_model_meta',
-    'EXT:cs_csseo/Resources/Private/Language/locallang_csh_tx_csseo_domain_model_meta.xlf'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_csseo_domain_model_meta');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
-    'pages',
-    'EXT:cs_seo/Resources/Private/Language/locallang_csh_pages.xlf'
-);
-
-/**
- * Include Backend Module
- */
-if (TYPO3_MODE === 'BE') {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'Clickstorm.' . $_EXTKEY,
-        'web',
-        'mod1',
-        '',
-        [
-            'Module' => 'pageMeta, pageIndex, pageOpenGraph, pageTwitterCards, pageResults, pageEvaluation'
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/mod.svg',
-            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf',
-        ]
+$boot = function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        'tx_csseo_domain_model_meta',
+        'EXT:cs_csseo/Resources/Private/Language/locallang_csh_tx_csseo_domain_model_meta.xlf'
     );
-}
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_csseo_domain_model_meta');
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        'pages',
+        'EXT:cs_seo/Resources/Private/Language/locallang_csh_pages.xlf'
+    );
+
+    if (TYPO3_MODE === 'BE') {
+        /**
+         * Include Backend Module
+         */
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'cs_seo',
+            'web',
+            'mod1',
+            '',
+            [
+                \Clickstorm\CsSeo\Controller\ModuleController::class => 'pageMeta, pageIndex, pageOpenGraph, pageTwitterCards, pageResults, pageEvaluation'
+            ],
+            [
+                'access' => 'user,group',
+                'icon' => 'EXT:cs_seo/Resources/Public/Icons/mod.svg',
+                'labels' => 'LLL:EXT:cs_seo/Resources/Private/Language/locallang.xlf',
+            ]
+        );
+    }
+};
+
+$boot();
+unset($boot);
