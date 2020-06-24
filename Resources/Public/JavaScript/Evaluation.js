7c534811a3886d8e36117fedd01a6f323d7a5e51
akoenig <koenig@clickstorm.de>
2020-06-24 15:29:32 +0200

    [TASK] Rework Evaluation.js to allow dependency via RequireJS

--- Resources/Public/JavaScript/Evaluation.js
+++ Resources/Public/JavaScript/Evaluation.js
@@ -6,83 +6,91 @@
  * @license clickstorm GmbH
  */
 define(['jquery', 'TYPO3/CMS/CsSeo/Cookie'], function($, Cookie) {
-    $(document).ready(function(){
+	var Evaluation = {};
 
-        // evaluation update
-        $('#cs-seo-evaluate').click(function(key, value) {
-            var $this = $(this),
-                $evaluateButton = $('#cs-seo-evaluate');
+	Evaluation.init = function() {
+		// evaluation update
+		$('#cs-seo-evaluate').click(function(key, value) {
+			var $this = $(this),
+				$evaluateButton = $('#cs-seo-evaluate');
 
-            $evaluateButton.before('<p class="cs-wait">...</p>');
-            $.post(
-                TYPO3.settings.ajaxUrls['tx_csseo_evaluate'],
-                {
-                    uid: $this.data('uid'),
-                    table: $this.data('table')
-                }
-                ).done(function(response){
-                    if(response.length > 0) {
-                        if(top.TYPO3.Notification) {
-                            var message = $(response).find('.alert').first().text();
-                            top.TYPO3.Notification.error('Not Updated', message, 5);
-                        } else {
-                            var message = $(response).find('.message-body').first().text();
-                            top.TYPO3.Flashmessage.display(4, 'Not Updated', message);
-                        }
-                        $('.cs-wait').remove();
-                        $evaluateButton.show();
-                    } else {
-                        if(top.TYPO3.Notification) {
-                            top.TYPO3.Notification.success('Updated', '', 3);
-                        } else {
-                            top.TYPO3.Flashmessage.display(2, 'Updated', '', 3);
-                        }
-                        location.reload();
-                    }
-            });
-            $evaluateButton.hide();
-            return false;
-        });
+			$evaluateButton.before('<p class="cs-wait">...</p>');
+			$.post(
+				TYPO3.settings.ajaxUrls['tx_csseo_evaluate'],
+				{
+					uid: $this.data('uid'),
+					table: $this.data('table')
+				}
+			).done(function(response){
+				if(response.length > 0) {
+					if(top.TYPO3.Notification) {
+						var message = $(response).find('.alert').first().text();
+						top.TYPO3.Notification.error('Not Updated', message, 5);
+					} else {
+						var message = $(response).find('.message-body').first().text();
+						top.TYPO3.Flashmessage.display(4, 'Not Updated', message);
+					}
+					$('.cs-wait').remove();
+					$evaluateButton.show();
+				} else {
+					if(top.TYPO3.Notification) {
+						top.TYPO3.Notification.success('Updated', '', 3);
+					} else {
+						top.TYPO3.Flashmessage.display(2, 'Updated', '', 3);
+					}
+					location.reload();
+				}
+			});
+			$evaluateButton.hide();
+			return false;
+		});
 
-        // toggle accordion
-        var $toggles = $('.js-csseo-toggle');
-        if($toggles.length > 0) {
-            $toggles.each(function() {
-	            var $toggle = $(this),
-                    $content = $($toggle.data('content')),
-		            showResults = false,
-		            useCookies = $toggle.data('cookie');
+		// toggle accordion
+		var $toggles = $('.js-csseo-toggle');
+		if($toggles.length > 0) {
+			$toggles.each(function() {
+				var $toggle = $(this),
+					$content = $($toggle.data('content')),
+					showResults = false,
+					useCookies = $toggle.data('cookie');
 
-	            if(useCookies) {
-		            showResults = Cookie.cookie('seo-results') == 1 ? true : false;
-	            }
+				if(useCookies) {
+					showResults = Cookie.cookie('seo-results') == 1 ? true : false;
+				}
 
-	            function toggleResults() {
-		            $content.toggle(showResults);
-		            $toggle.toggleClass('csseo-icon-up-open', showResults);
-		            $toggle.toggleClass('csseo-icon-down-open', !showResults);
-	            }
+				function toggleResults() {
+					$content.toggle(showResults);
+					$toggle.toggleClass('csseo-icon-up-open', showResults);
+					$toggle.toggleClass('csseo-icon-down-open', !showResults);
+				}
 
-	            $toggle.click(function() {
-		            showResults = !showResults;
-		            toggleResults();
-		            if(useCookies) {
-			            Cookie.cookie('seo-results', showResults ? 1 : 0);
-		            }
-	            });
+				$toggle.click(function() {
+					showResults = !showResults;
+					toggleResults();
+					if(useCookies) {
+						Cookie.cookie('seo-results', showResults ? 1 : 0);
+					}
+				});
 
-	            if(!showResults) {
-		            toggleResults();
-	            }
-            });
-        }
+				if(!showResults) {
+					toggleResults();
+				}
+			});
+		}
 
-        // record selector with search box
-        var $recordSelector = $('#cs-record');
-        if($recordSelector.length > 0) {
-	        $recordSelector.select2();
-	        $recordSelector.find('option[value=""]:not(:selected)').remove();
-        }
+		// record selector with search box
+		var $recordSelector = $('#cs-record');
+		if($recordSelector.length > 0) {
+			$recordSelector.select2();
+			$recordSelector.find('option[value=""]:not(:selected)').remove();
+		}
+	}
 
-    });
+	// Call the init function when document is ready
+	$(document).ready(function(){
+		Evaluation.init();
+	});
+
+	// To let the module be a dependency of another module, we return our object
+	return Evaluation;
 });
