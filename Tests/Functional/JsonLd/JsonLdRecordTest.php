2db698eab806c61f15543ce27c6d6b38a294b899
hirdes <hirdes@clickstorm.de>
2021-05-31 09:41:10 +0200

    [TASK] json-ld tests

---
+++ Tests/Functional/JsonLd/JsonLdRecordTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clickstorm\CsSeo\Tests\Functional\JsonLd;
+
+class JsonLdRecordTest extends AbstractJsonLdTest
+{
+    public function ensureMetaDataAreCorrectDataProvider(): array
+    {
+        return [
+            'category 1: with title and description fallback' => [
+                'http://localhost/category/1',
+                ''
+            ],
+            'category 2: hidden metadata' => [
+                'http://localhost/category/2',
+                ''
+            ],
+            'category 3: full metadata' => [
+                'http://localhost/category/3',
+                '{"@context": "https://schema.org","@type": "Organization","url": "https://www.json-ld-test.com"}'
+            ]
+        ];
+    }
+}
---
+++ Tests/Functional/JsonLd/JsonLdTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Clickstorm\CsSeo\Tests\Functional\JsonLd;
+
+class JsonLdTest extends AbstractJsonLdTest
+{
+    public function ensureMetaDataAreCorrectDataProvider(): array
+    {
+        return [
+            'page 1: with JSON-LD' => [
+                'http://localhost/',
+                '{"@context": "https://schema.org","@type": "Organization","url": "https://www.json-ld-test.com"}'
+            ],
+            'page 2: without JSON-LD' => [
+                'http://localhost/page-without-json-ld',
+                ''
+            ],
+        ];
+    }
+}
