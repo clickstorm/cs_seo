c35d68d57f3160a66dbc74476a11d8ceb8e2045b
hirdes <hirdes@clickstorm.de>
2021-05-31 15:30:18 +0200

    [FEATURE] add more information to evalutaion event

--- Classes/Event/ModifyEvaluationPidEvent.php
+++ Classes/Event/ModifyEvaluationPidEvent.php
@@ -14,6 +14,12 @@ final class ModifyEvaluationPidEvent
      */
     private $pid;
 
+    protected $params;
+
+    protected $tableName;
+
+    protected $pageInfo;
+
     /**
      * ModifyEvaluationPidEvent constructor.
      * @param int $pid page id to call and modify
@@ -24,6 +30,9 @@ final class ModifyEvaluationPidEvent
     public function __construct(int $pid, string $params, string $tableName, array $pageInfo)
     {
         $this->pid = $pid;
+        $this->params = $params;
+        $this->tableName = $tableName;
+        $this->pageInfo = $pageInfo;
     }
 
     /**
@@ -40,5 +49,53 @@ final class ModifyEvaluationPidEvent
     public function setPid(int $pid): void
     {
         $this->pid = $pid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParams(): string
+    {
+        return $this->params;
+    }
+
+    /**
+     * @param string $params
+     */
+    public function setParams(string $params): void
+    {
+        $this->params = $params;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableName(): string
+    {
+        return $this->tableName;
+    }
+
+    /**
+     * @param string $tableName
+     */
+    public function setTableName(string $tableName): void
+    {
+        $this->tableName = $tableName;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPageInfo(): array
+    {
+        return $this->pageInfo;
+    }
+
+    /**
+     * @param array $pageInfo
+     */
+    public function setPageInfo(array $pageInfo): void
+    {
+        $this->pageInfo = $pageInfo;
     }
 }
