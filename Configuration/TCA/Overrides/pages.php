19c0b5d17ffc149ab12b57c493e507ba0554da07
mhirdes <hirdes@clickstorm.de>
2022-04-06 11:55:29 +0200

    [BUGFIX] #286 fix TCA position of sitemap fields

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -66,13 +66,6 @@ $tempColumns = [
 $GLOBALS['TCA']['pages']['palettes']['metatags']['showitem'] =
     preg_replace('/description(.*,|.*$)/', '', $GLOBALS['TCA']['pages']['palettes']['metatags']['showitem']);
 
-foreach ($GLOBALS['TCA']['pages']['types'] as $key => $type) {
-    $type['showitem'] =
-        str_replace('--palette--;;opengraph,', '', $type['showitem']);
-    $GLOBALS['TCA']['pages']['types'][$key]['showitem'] =
-        str_replace('--palette--;;twittercards,', '', $type['showitem']);
-}
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
     'pages',
     'metatags',
