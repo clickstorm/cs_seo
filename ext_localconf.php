b76b995d98ad14e614965e31b390da86758a72b9
mhirdes <hirdes@clickstorm.de>
2022-04-06 14:36:29 +0200

    [BUGFIX] remove deprecated TYPO3 constants

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,43 +2,38 @@
 defined('TYPO3') || die();
 
 (function () {
-    if ((TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL) === 0) {
-        $confArray = \Clickstorm\CsSeo\Utility\ConfigurationUtility::getEmConfiguration();
+    $confArray = \Clickstorm\CsSeo\Utility\ConfigurationUtility::getEmConfiguration();
 
-        if (TYPO3_MODE === 'BE') {
+    // Hook into the page module
+    if (!isset($confArray['inPageModule']) || $confArray['inPageModule'] < 2) {
+        $hook = ($confArray['inPageModule'] == 1) ? 'drawFooterHook' : 'drawHeaderHook';
 
-            // Hook into the page module
-            if (!isset($confArray['inPageModule']) || $confArray['inPageModule'] < 2) {
-                $hook = ($confArray['inPageModule'] == 1) ? 'drawFooterHook' : 'drawHeaderHook';
-
-                $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php'][$hook]['cs_seo'] =
-                    \Clickstorm\CsSeo\Hook\PageHook::class . '->render';
-            }
-
-            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Clickstorm\\CsSeo\\Evaluation\\TCA\\RobotsDisallowAllEvaluator'] = '';
-            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Clickstorm\\CsSeo\\Evaluation\\TCA\\RobotsExistsEvaluator'] = '';
-        }
-
-        // new field types
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1524490067] = [
-            'nodeName' => 'snippetPreview',
-            'priority' => 30,
-            'class' => \Clickstorm\CsSeo\Form\Element\SnippetPreview::class,
-        ];
-
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1620117622] = [
-            'nodeName' => 'txCsseoJsonLd',
-            'priority' => 30,
-            'class' => \Clickstorm\CsSeo\Form\Element\JsonLdElement::class,
-        ];
-
-        // Register the class to be available in 'eval' of TCA
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Clickstorm\\CsSeo\\Evaluation\\TCA\\JsonLdEvaluator'] = '';
-
-        // add hook to get current cHash params
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['getData']['cs_seo'] =
-            \Clickstorm\CsSeo\Hook\CurrentUrlGetDataHook::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php'][$hook]['cs_seo'] =
+            \Clickstorm\CsSeo\Hook\PageHook::class . '->render';
     }
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Clickstorm\\CsSeo\\Evaluation\\TCA\\RobotsDisallowAllEvaluator'] = '';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Clickstorm\\CsSeo\\Evaluation\\TCA\\RobotsExistsEvaluator'] = '';
+
+    // new field types
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1524490067] = [
+        'nodeName' => 'snippetPreview',
+        'priority' => 30,
+        'class' => \Clickstorm\CsSeo\Form\Element\SnippetPreview::class,
+    ];
+
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1620117622] = [
+        'nodeName' => 'txCsseoJsonLd',
+        'priority' => 30,
+        'class' => \Clickstorm\CsSeo\Form\Element\JsonLdElement::class,
+    ];
+
+    // Register the class to be available in 'eval' of TCA
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Clickstorm\\CsSeo\\Evaluation\\TCA\\JsonLdEvaluator'] = '';
+
+    // add hook to get current cHash params
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['getData']['cs_seo'] =
+        \Clickstorm\CsSeo\Hook\CurrentUrlGetDataHook::class;
 
     // generate and overwrite header data
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['TYPO3\CMS\Frontend\Page\PageGenerator']['generateMetaTags'][] =
--- ext_tables.php
+++ ext_tables.php
@@ -20,39 +20,37 @@ defined('TYPO3') || die();
         'EXT:cs_seo/Resources/Private/Language/locallang_csh_pages.xlf'
     );
 
-    if (TYPO3_MODE === 'BE') {
-        /**
-         * Include Backend Module
-         */
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-            'cs_seo',
-            'web',
-            'mod1',
-            '',
-            [
-                \Clickstorm\CsSeo\Controller\ModuleWebController::class =>
-                    'pageMeta, pageIndex, pageOpenGraph, pageTwitterCards, pageStructuredData, pageResults, pageEvaluation'
-            ],
-            [
-                'access' => 'user,group',
-                'icon' => 'EXT:cs_seo/Resources/Public/Icons/mod.svg',
-                'labels' => 'LLL:EXT:cs_seo/Resources/Private/Language/Module/web.xlf',
-            ]
-        );
+    /**
+     * Include Backend Module
+     */
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'cs_seo',
+        'web',
+        'mod1',
+        '',
+        [
+            \Clickstorm\CsSeo\Controller\ModuleWebController::class =>
+                'pageMeta, pageIndex, pageOpenGraph, pageTwitterCards, pageStructuredData, pageResults, pageEvaluation'
+        ],
+        [
+            'access' => 'user,group',
+            'icon' => 'EXT:cs_seo/Resources/Public/Icons/mod.svg',
+            'labels' => 'LLL:EXT:cs_seo/Resources/Private/Language/Module/web.xlf',
+        ]
+    );
 
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-            'cs_seo',
-            'file',
-            'mod_file',
-            'bottom',
-            [
-                \Clickstorm\CsSeo\Controller\ModuleFileController::class => 'showEmptyImageAlt,update'
-            ],
-            [
-                'access' => 'user,group',
-                'icon' => 'EXT:cs_seo/Resources/Public/Icons/modFile.svg',
-                'labels' => 'LLL:EXT:cs_seo/Resources/Private/Language/Module/file.xlf',
-            ]
-        );
-    }
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'cs_seo',
+        'file',
+        'mod_file',
+        'bottom',
+        [
+            \Clickstorm\CsSeo\Controller\ModuleFileController::class => 'showEmptyImageAlt,update'
+        ],
+        [
+            'access' => 'user,group',
+            'icon' => 'EXT:cs_seo/Resources/Public/Icons/modFile.svg',
+            'labels' => 'LLL:EXT:cs_seo/Resources/Private/Language/Module/file.xlf',
+        ]
+    );
 })();
