8ee1e256e34ad02b132d816303e79cc901041305
mhirdes <hirdes@clickstorm.de>
2022-10-28 17:09:26 +0200

    [BUGFIX] register backend modules again

---
+++ Configuration/Backend/Modules.php
@@ -0,0 +1,34 @@
+<?php
+
+use Clickstorm\CsSeo\Controller\ModuleWebController;
+use Clickstorm\CsSeo\Controller\ModuleFileController;
+
+return [
+    'web_CsSeo' => [
+        'parent' => 'web',
+        'position' => ['after' => '*'],
+        'access' => 'user',
+        'workspaces' => 'live',
+        'iconIdentifier' => 'tx-cssseo-module-web',
+        'path' => '/module/web/cs-seo',
+        'labels' => 'LLL:EXT:cs_seo/Resources/Private/Language/Module/web.xlf',
+        'extensionName' => 'CsSeo',
+        'controllerActions' => [
+            ModuleWebController::class =>
+                'pageMeta, pageIndex, pageOpenGraph, pageTwitterCards, pageStructuredData, pageResults, pageEvaluation',
+        ],
+    ],
+    'file_CsSeo' => [
+        'parent' => 'file',
+        'position' => ['after' => '*'],
+        'access' => 'user',
+        'workspaces' => 'live',
+        'iconIdentifier' => 'tx-cssseo-module-file',
+        'path' => '/module/file/cs-seo',
+        'labels' => 'LLL:EXT:cs_seo/Resources/Private/Language/Module/web.xlf',
+        'extensionName' => 'CsSeo',
+        'controllerActions' => [
+            ModuleFileController::class => 'showEmptyImageAlt,update',
+        ],
+    ],
+];
---
+++ Configuration/Icons.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'tx-cssseo-module-web' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:cs_seo/Resources/Public/Icons/mod.svg',
+    ],
+    'tx-cssseo-module-file' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:cs_seo/Resources/Public/Icons/modFile.svg',
+    ],
+];
