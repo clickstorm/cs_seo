bc41a911d08d6ea03bfe6e5ab3b701748ab321ae
mhirdes <Marc_Hirdes@gmx.de>
2024-06-04 16:49:54 +0200

    [TASK] update requirements for TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,11 +12,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'clickstorm GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '8.2.1-dev',
+    'version' => '9.0.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.2.0-12.4.99',
-            'seo' => '12.2.0-12.4.99',
+            'typo3' => '13.1.1-13.4.99',
+            'seo' => '12.3.1-13.4.99',
         ],
     ],
     'autoload' => [
