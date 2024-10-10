a3ae5a7431f1df917bc3e25b686464a163f7a708
mhirdes <hirdes@clickstorm.de>
2024-10-10 10:00:24 +0200

    [BUGFIX] evaluation module index

--- Classes/Service/Backend/GridService.php
+++ Classes/Service/Backend/GridService.php
@@ -135,10 +135,15 @@ class GridService
                 GlobalsUtility::getLanguageService()->sL($GLOBALS['TCA']['pages']['columns'][$fieldName]['label']);
             switch ($GLOBALS['TCA']['pages']['columns'][$fieldName]['config']['type']) {
                 case 'check':
+                    $cellTemplateValue = '{{row.entity[col.field] == true ? \'☑\' : \'☐\'}}';
+                    if(!empty($GLOBALS['TCA']['pages']['columns'][$fieldName]['config']['items'][0]['invertStateDisplay'])) {
+                        $columnDef['displayName'] =
+                            GlobalsUtility::getLanguageService()->sL('LLL:EXT:cs_seo/Resources/Private/Language/locallang_db.xlf:pages.tx_csseo_' . $fieldName);
+                    }
                     $columnDef['type'] = 'boolean';
                     $columnDef['width'] = 100;
                     $columnDef['cellTemplate'] =
-                        '<div class="ui-grid-cell-contents ng-binding ng-scope text-center">{{row.entity[col.field] == true ? \'☑\' : \'☐\'}}</span></div>';
+                        '<div class="ui-grid-cell-contents ng-binding ng-scope text-center">' . $cellTemplateValue . '</span></div>';
                     $columnDef['editableCellTemplate'] =
                         '<div><form name="inputForm" class="text-center"><input type="checkbox" ui-grid-editor ng-model="MODEL_COL_FIELD" ng-click="grid.appScope.currentValue = MODEL_COL_FIELD"></form></div>';
                     $columnDef['enableFiltering'] = false;
