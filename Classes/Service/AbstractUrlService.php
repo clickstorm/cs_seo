2076eb923893ec5c382a9fcf3872993a9f8b2515
mhirdes <hirdes@clickstorm.de>
2025-08-19 11:06:53 +0200

    [BUGFIX] prevent php warning in abstract URL service

--- Classes/Service/AbstractUrlService.php
+++ Classes/Service/AbstractUrlService.php
@@ -57,8 +57,8 @@ abstract class AbstractUrlService
         // first check if sys_language_uid of record is 0
         $select = implode(',', [$pointerField, $languageField, 'uid']);
         $currentRecord = DatabaseUtility::getRecord($table, $uid, $select);
-        $languageUidOfCurrentRecord = (int)$currentRecord[$languageField];
-        $l10nParentOfCurrentRecord = (int)$currentRecord[$pointerField];
+        $languageUidOfCurrentRecord = (int)($currentRecord[$languageField] ?? 0);
+        $l10nParentOfCurrentRecord = (int)($currentRecord[$pointerField] ?? 0);
 
         // if languageUid of current record is not default and l10nParen is set, use the uid of the default language record
         if ($languageUidOfCurrentRecord > 0 && $l10nParentOfCurrentRecord) {
